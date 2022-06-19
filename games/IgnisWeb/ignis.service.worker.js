da8aa745f7bd282e7b2ad60065c591b390fc635c
thepromaper <davidcampeon2007@gmail.com>
2022-06-19 13:40:03 -0500

    ignis web 2

--- games/IgnisWeb/ignis.service.worker.js
+++ games/IgnisWeb/ignis.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1655663163|539405940";
+const CACHE_VERSION = "1655663964|1340302227";
 const CACHE_PREFIX = "ignis-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "ignis.offline.html";
