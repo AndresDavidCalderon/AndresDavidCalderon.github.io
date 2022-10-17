4f3affa427edd8909f397bab69ad95d56faed943
thepromaper <davidcampeon2007@gmail.com>
2022-10-17 10:03:23 -0500

    working buttons

---
+++ index.js
@@ -0,0 +1,11 @@
+document.getElementById("github").addEventListener("click",()=>{
+    location.href="https://github.com/thepromaper"
+});
+
+document.getElementById("google-play").addEventListener("click",()=>{
+    location.href="https://play.google.com/store/apps/dev?id=5291594949000156574"
+})
+
+document.getElementById("fiverr").addEventListener("click",()=>{
+    location.href="https://www.fiverr.com/void_games"
+})
