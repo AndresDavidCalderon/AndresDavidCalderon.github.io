e405876ba2af69def3a0ab6dc874d19a099bc153
thepromaper <davidcampeon2007@gmail.com>
2023-03-16 21:12:34 -0500

    deleted all.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 document.getElementById("github").addEventListener("click",()=>{
-    location.href="https://github.com/thepromaper"
+    window.open("https://github.com/thepromaper")
 });
 
 document.getElementById("google-play").addEventListener("click",()=>{
-    location.href="https://play.google.com/store/apps/dev?id=5291594949000156574"
+    window.open("https://play.google.com/store/apps/dev?id=5291594949000156574")
 })
 
 document.getElementById("fiverr").addEventListener("click",()=>{
-    location.href="https://www.fiverr.com/void_games"
+    window.open("https://www.fiverr.com/void_games")
 })
